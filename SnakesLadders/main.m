d2a9bc3c1985702315f9a5862fdc308e4d796524
Auston <auston.salvana@live.com>
2015-07-06 06:16:14 -0700

    Initial Commit

---
+++ SnakesLadders/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SnakesLadders
+//
+//  Created by Auston Salvana on 7/6/15.
+//  Copyright (c) 2015 ASolo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
